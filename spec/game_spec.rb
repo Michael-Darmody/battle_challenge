a5cbc9f05379bd78d03c8eb783a869d3a93601fa
Michael <darmodymichael@gmail.com>
2019-07-02 14:47:28 +0100

    Refactored Game class to take Player instances, extracted logic from controller into the model

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,24 @@
 require 'game'
 
 describe Game do
-  let(:subject) { described_class.new }
+  let(:subject) { described_class.new(player_1, player_2) }
   let(:player_1) { double(:mike) }
   let(:player_2) { double(:mark) }
 
+  describe '#player_1' do
+    it 'retrieves player 1' do
+      expect(subject.player_1).to eq(player_1)
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves player 2' do
+      expect(subject.player_2).to eq(player_2)
+    end
+  end
+
   describe '#attack' do
-    it 'attacks the opposing player' do
+    it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       subject.attack(player_2)
     end
