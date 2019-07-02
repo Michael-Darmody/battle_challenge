aa754f622dffebcb016bb4de9d01ca8991afc81d
Michael <darmodymichael@gmail.com>
2019-07-02 12:45:48 +0100

    Implememnted ability to reduce HP, refactored controller, passed test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,4 +5,12 @@ feature 'Attacking a player' do
     click_button('Attack')
     expect(page).to have_content 'Mike attacked Mark!'
   end
+
+  scenario 'reduce player 2 HP by 10' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Ok')
+    expect(page).not_to have_content 'Mark: 100HP'
+    expect(page).to have_content 'Mark: 90HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,31 @@
 require 'player'
 
 describe Player do
-  let(:subject) { described_class.new('Mike') }
+  let(:mike) { described_class.new('Mike') }
+  let(:mark) { described_class.new('Mark') }
 
   describe '#name' do
     it 'returns the player name' do
-      expect(subject.name).to eq('Mike')
+      expect(mike.name).to eq('Mike')
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the players HP' do
+      expect(mike.hit_points).to eq(Player::DEFAULT_HP)
+    end
+  end
+
+  describe '#attack' do
+    it 'attacks the opposing player' do
+      expect(mark).to receive(:receive_damage)
+      mike.attack(mark)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'damages the opposing player' do
+      expect{ mike.receive_damage }.to change{ mike.hit_points }.by(-10)
     end
   end
 end
