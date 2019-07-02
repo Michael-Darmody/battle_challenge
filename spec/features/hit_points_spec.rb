4c24cb4fe194eab8e1112eaab0ca7e425dfe5027
Michael <darmodymichael@gmail.com>
2019-07-02 19:05:13 +0100

    Created a feature test and updated view for seeing other players HP, passed test

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,10 @@
 
 feature 'Viewing HP' do
+  scenario 'can see player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Mike: 100HP'
+  end
+
   scenario 'can see player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Mark: 100HP'
